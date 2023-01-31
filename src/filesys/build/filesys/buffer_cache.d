c704ad293dc0faa3828109ed382988544f22d4cb
pham duy <duypham2942001@gmail.com>
2023-01-31 14:18:18 +0700

    add code buffer cache

---
+++ src/filesys/build/filesys/buffer_cache.d
@@ -0,0 +1,5 @@
+filesys/buffer_cache.o: ../../filesys/buffer_cache.c ../../lib/debug.h \
+ ../../lib/string.h ../../lib/stddef.h ../../filesys/buffer_cache.h \
+ ../../devices/block.h ../../lib/inttypes.h ../../lib/stdint.h \
+ ../../filesys/filesys.h ../../lib/stdbool.h ../../filesys/off_t.h \
+ ../../threads/synch.h ../../lib/kernel/list.h
